fa254be1880dda99a37f4b120233f39e92816add
gmuryan <gabrielmuryan@gmail.com>
2019-04-26 00:37:18 -0300

    todoTalleres

--- src/main/java/com/uade/todoTalleres/TodoTalleresApplication.java
+++ src/main/java/com/uade/todoTalleres/TodoTalleresApplication.java
@@ -1,14 +1,13 @@
-package com.fabricaReact;
+package com.uade.todoTalleres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FabricaReactApplication {
+public class TodoTalleresApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FabricaReactApplication.class, args);
+		SpringApplication.run(TodoTalleresApplication.class, args);
 	}
 
 }
-
