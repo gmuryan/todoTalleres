f355066f4e4c784f45711c3e8be663c82a417a5f
gmuryan <gabrielmuryan@gmail.com>
2019-04-29 11:26:36 -0300

    Agrego validacion del form del cliente

--- ui/app/src/ClienteEdit.js
+++ ui/app/src/ClienteEdit.js
@@ -18,7 +18,8 @@ class ClienteEdit extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            item: this.emptyItem
+            item: this.emptyItem,
+            errors: {}
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -40,8 +41,69 @@ class ClienteEdit extends Component {
         this.setState({item});
     }
 
+    handleValidation(){
+        let fields = this.state.item;
+        let errors = {};
+        let formIsValid = true;
+
+        //Name
+        if(fields["nombre"].length === 0){
+            formIsValid = false;
+            errors["nombre"] = "No puede estar vacio";
+        }
+        else if(typeof fields["nombre"] !== "undefined"){
+            if(!fields["nombre"].match(/^[a-zA-Z]+$/)){
+                formIsValid = false;
+                errors["nombre"] = "Solo letras";
+            }
+        }
+
+        //Apellido
+        if(fields["apellido"].length === 0){
+            formIsValid = false;
+            errors["apellido"] = "No puede estar vacio";
+        }
+        else if(typeof fields["apellido"] !== "undefined"){
+            if(!fields["apellido"].match(/^[a-zA-Z]+$/)){
+                formIsValid = false;
+                errors["apellido"] = "Solo letras";
+            }
+        }
+
+        //Telefono
+        if(fields["telefono"].length === 0){
+            formIsValid = false;
+            errors["telefono"] = "No puede estar vacio";
+        }
+        else if(typeof fields["telefono"] !== "undefined"){
+            if(!fields["telefono"].match(/^[0-9]+$/)){
+                formIsValid = false;
+                errors["telefono"] = "Solo numeros";
+            }
+        }
+
+        //Email
+        if(!fields["mail"]){
+            formIsValid = false;
+            errors["mail"] = "No puede estar vacio";
+        }
+        else if(typeof fields["mail"] !== "undefined"){
+            let lastAtPos = fields["mail"].lastIndexOf('@');
+            let lastDotPos = fields["mail"].lastIndexOf('.');
+
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["mail"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["mail"].length - lastDotPos) > 2)) {
+                formIsValid = false;
+                errors["mail"] = "Mail invalido";
+            }
+        }
+
+        this.setState({errors: errors});
+        return formIsValid;
+    }
+
     async handleSubmit(event) {
         event.preventDefault();
+        if (this.handleValidation()){
         const {item} = this.state;
 
         await fetch('/api/cliente', {
@@ -53,6 +115,7 @@ class ClienteEdit extends Component {
             body: JSON.stringify(item),
         });
         this.props.history.push('/clientes');
+        }
     }
 
     render() {
@@ -68,21 +131,25 @@ class ClienteEdit extends Component {
                         <Label for="nombre">Nombre</Label>
                         <Input type="text" name="nombre" id="nombre" value={item.nombre || ''}
                                onChange={this.handleChange} autoComplete="nombre"/>
+                        <span className="error">{this.state.errors["nombre"]}</span>
                     </FormGroup>
                     <FormGroup>
                         <Label for="apellido">Apellido</Label>
                         <Input type="text" name="apellido" id="apellido" value={item.apellido || ''}
                                onChange={this.handleChange} autoComplete="apellido"/>
+                        <span className="error">{this.state.errors["apellido"]}</span>
                     </FormGroup>
                     <FormGroup>
                         <Label for="telefono">Telefono</Label>
                         <Input type="text" name="telefono" id="telefono" value={item.telefono || ''}
                                onChange={this.handleChange} autoComplete="telefono"/>
+                        <span className="error">{this.state.errors["telefono"]}</span>
                     </FormGroup>
                     <FormGroup>
                         <Label for="mail">Mail</Label>
                         <Input type="text" name="mail" id="mail" value={item.mail || ''}
                                onChange={this.handleChange} autoComplete="mail"/>
+                        <span className="error">{this.state.errors["mail"]}</span>
                     </FormGroup>
                     <FormGroup>
                         <Button color="primary" type="submit">Guardar</Button>{' '}
